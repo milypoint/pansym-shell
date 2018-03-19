7cea75604ff41bc3cef4c518ea06f283eff41560
Andrii Tokar <milypointo@gmail.com>
2018-03-19 15:24:31 +0200

    Глобальные изменения файла readres.py
    Добавлена функция sortByGroup для гриппирования данных по признакам и сортировки каждой группы по ключу.
    
    Изменена функция calcEfficiency - теперь код в более короткой и понятно форме.
    
    Изменен блок с генерацией основных графиков. Сам блок перенесен в функцию.
    
    Блок для генерации графиком моментов от угла также перенесен в отдельную функцию.
    
    Создана функция mkdir для проверки наличия определенной папки и ее создания в случае ее отсутствия.

--- tools/readres.py
+++ tools/readres.py
@@ -6,7 +6,18 @@ import glob
 from rotate_foilpoints import *
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
+from itertools import groupby
+from operator import itemgetter
+
+def iprint(data, split=False):
+	for item in data:
+		print(item)
+		if split:
+			print()
+
+def mkdir(dir_name):
+	if not os.path.exists(dir_name):
+		os.makedirs(dir_name)
 
 def fileNameDecode(fname):
 	'''
@@ -28,6 +39,33 @@ def isChangeData(cur_data, last_data):
 		if cur_data[key] != value: return True
 	return False
 
+def sortByGroup(data, group_keys, sort_key=None, split=False):
+	'''
+	Групирует данные по ключам group_keys, сортирует группы по ключу sort_key (опционально).
+
+	Вход:
+		data - список словарей. Словари должны иметь все ключи, в ходящие в признак группы. 
+		group_keys - список ключей словаря, по которым происходит группирование
+		sort_key - необязательный параметр, ключ сортировки внутри каждой группы
+		split - если True, то возвращается список, содержащий группы словарей. Иначе возвращается список словарей.
+
+	Выход:
+		Список словарей, упорядоченный по (отсортированным группам, если указан ключ сортировки) группам 
+	'''
+	grouper = itemgetter(*group_keys)
+	grouped_data = []
+
+	for key, grp in groupby(sorted(data, key = grouper), grouper):
+		group = [i for i in grp]
+		if sort_key != None:
+			group.sort(key = lambda k: k[sort_key])
+		if split == False:
+			grouped_data += group
+		else:
+			grouped_data.append(group)
+
+	return grouped_data
+
 def calcEfficiency(data):
 	'''
 	Расчитывает еффективность руля по координатам ох и oz
@@ -38,73 +76,137 @@ def calcEfficiency(data):
 
 	Возвращает массив словарей с расчитанной еффективностю относительно соответствующий осей
 	'''
+	#Группируем данные, сортируем по углу отклонения и разделяем группы:
+	groups = sortByGroup(data, ['rudpos', 'backwingspan', 'ruderratio'], 'delta', True)
+
 	ndata = []
-	last_data = dict()
 
-	for item in data:
-		if item == data[0]: #если первый словарь из массива, то создать объекты данных
-			d, mx, mz = [], [], []
-			for key, value in item.items():
-				if key != 'delta' and key != 'coefs':
-					last_data [key] = value
+	for group in groups:
+		d, mx, mz = [], [], []
+		temp_dict = { k:v for k,v in group[0].items() if k != 'delta' and k != 'coefs'}
+		for item in group:
+			d.append(item['delta'])
+			mx.append(item['coefs'][3])
+			mz.append(item['coefs'][5])
+		#Сдвигаем значения коэф. момента на величину равную коэф. при нулевом отклонении руля
+		#где [i for i,x in enumerate(d) if x == 0][0] - определение индекса массива нулевого отклонения
+		mx = [round(x - mx[ [i for i,x in enumerate(d) if x == 0][0] ], 4) for x in mx]
+		mz = [round(x - mz[ [i for i,x in enumerate(d) if x == 0][0] ], 4) for x in mz]
+		#список индексов точек, входящих в аппроксимацию:
+		aprx_i = [i for i,x in enumerate(d) if x >=-10 and x <=10] 
+		#Списки точек, входящих в аппроксимацию:
+		aprx_d = d[aprx_i[0] : aprx_i[-1] + 1]
+		aprx_mx = mx[aprx_i[0] : aprx_i[-1] + 1]
+		aprx_mz = mz[aprx_i[0] : aprx_i[-1] + 1]
+		#Сохраняем данные:
+		temp_dict['kx'] = round(np.polyfit(aprx_d, aprx_mx, 1)[0], 4) 
+		temp_dict['kz'] = round(np.polyfit(aprx_d, aprx_mz, 1)[0], 4) 
+		temp_dict['d'] = d
+		temp_dict['mx'] = mx
+		temp_dict['mz'] = mz
+		temp_dict['aprx_d'] = aprx_d
+		temp_dict['aprx_mx'] = aprx_mx
+		temp_dict['aprx_mz'] = aprx_mz
+		ndata.append(temp_dict)
 
-		if isChangeData(item, last_data) == True: #если данные, которые не входят в расчет еффективностей, 
-			#изменились, то считаем еффективность
-			new_item = dict()
-			for key, value in last_data.items():
-				new_item [key] = value
-				last_data [key] = item[key]
-			#Сдвигаем значения коэф. момента на величину равную коэф. при нулевом отклонении руля
-			#где [i for i,x in enumerate(d) if x == 0][0] - определение индекса массива нулевого отклонения
-			mx = [x - mx[ [i for i,x in enumerate(d) if x == 0][0] ] for x in mx]
-			mz = [x - mz[ [i for i,x in enumerate(d) if x == 0][0] ] for x in mz]
-
-			aprx_i = [i for i,x in enumerate(d) if x >=-10 and x <=10]
-
-			aprx_d = d[aprx_i[0] : aprx_i[-1] + 1]
-			aprx_mx = mx[aprx_i[0] : aprx_i[-1] + 1]
-			aprx_mz = mz[aprx_i[0] : aprx_i[-1] + 1]
-
-			#аппроксимируем данные прямой и получаем коэф. ее наклона
-			new_item['kx'] = round(np.polyfit(aprx_d, aprx_mx, 1)[0], 4) 
-			new_item['kz'] = round(np.polyfit(aprx_d, aprx_mz, 1)[0], 4) 
-			new_item['d'] = d
-			new_item['mx'] = mx
-			new_item['mz'] = mz
-			new_item['aprx_d'] = aprx_d
-			new_item['aprx_mx'] = aprx_mx
-			new_item['aprx_mz'] = aprx_mz
-			ndata.append(new_item)
-			d, mx, mz = [], [], []
-
-		d.append(item['delta'])
-		mx.append(item['coefs'][3])
-		mz.append(item['coefs'][5])
-
-		if item == data[-1]: #если последний словарь из массива, то считаем еффективность
-			new_item = dict()
-			for key, value in last_data.items():
-				new_item [key] = value
-				last_data [key] = item[key]
-			#
-			mx = [x - mx[ [i for i,x in enumerate(d) if x == 0][0] ] for x in mx]
-			mz = [x - mz[ [i for i,x in enumerate(d) if x == 0][0] ] for x in mz]
-			#массивы точек входящие в диапазон аппроксимации
-			aprx_d = d[ [i for i,x in enumerate(d) if x >=-10 and x <=10][0] : [i for i,x in enumerate(d) if x >=-10 and x <=10][-1] + 1] 
-			aprx_mx = mx[ [i for i,x in enumerate(d) if x >=-10 and x <=10][0] : [i for i,x in enumerate(d) if x >=-10 and x <=10][-1] + 1]
-			aprx_mz = mz[ [i for i,x in enumerate(d) if x >=-10 and x <=10][0] : [i for i,x in enumerate(d) if x >=-10 and x <=10][-1] + 1]
-			#
-			new_item['kx'] = round(np.polyfit(aprx_d, aprx_mx, 1)[0], 4) 
-			new_item['kz'] = round(np.polyfit(aprx_d, aprx_mz, 1)[0], 4) 
-			new_item['d'] = d
-			new_item['mx'] = mx
-			new_item['mz'] = mz
-			new_item['aprx_d'] = aprx_d
-			new_item['aprx_mx'] = aprx_mx
-			new_item['aprx_mz'] = aprx_mz
-			ndata.append(new_item)
-			
 	return ndata
+
+def genMainFigs(data):
+
+	#Группируем данные по ключам, которые не меняются для всех точек одной кривой
+	data = sortByGroup(data, ['rudpos', 'ruderratio'], split=True)
+	images_dir = getProjectDir() + 'data/images/' #директория, в которую записать графики
+	mkdir(images_dir)
+
+	for group in data:
+		x, kx, kz = [], [], []
+
+		for item in group:
+			x.append(abs(item['backwingspan']/1.25))
+			kx.append(abs(item['kx']))
+			kz.append(abs(item['kz']))
+
+		if group[0]['rudpos'] == 'front':
+			plt.figure(0)
+			plt.plot(x, kx, 'ro', x, kx, 'k')
+			plt.annotate(str(group[0]['ruderratio'])+'%', xy=(x[-1], kx[-1]), 
+				xytext=(x[-1]+abs(x[-1]*0.01), kx[-1]+abs(kx[-1]*0.01)))
+
+			plt.figure(1)
+			plt.plot(x, kz, 'ro', x, kz, 'k')
+			plt.annotate(str(group[0]['ruderratio'])+'%', xy=(x[-1], kz[-1]), 
+				xytext=(x[-1]+abs(x[-1]*0.01), kz[-1]+abs(kz[-1]*0.01)))
+		elif group[0]['rudpos'] == 'back':
+			plt.figure(2)
+			plt.plot(x, kx, 'ro', x, kx, 'k')
+			plt.annotate(str(group[0]['ruderratio'])+'%', xy=(x[-1], kx[-1]), 
+				xytext=(x[-1]+abs(x[-1]*0.01), kx[-1]+abs(kx[-1]*0.01)))
+
+			plt.figure(3)
+			plt.plot(x, kz, 'ro', x, kz, 'k')
+			plt.annotate(str(group[0]['ruderratio'])+'%', xy=(x[-1], kz[-1]), 
+				xytext=(x[-1]+abs(x[-1]*0.01), kz[-1]+abs(kz[-1]*0.01)))
+	
+	#Конфигурируем графики:
+
+	plt.figure(0)
+	plt.title('Эффективность элерона на переднем крыле')
+	plt.ylabel('eff_ox')
+	plt.xlabel('L2/L1')
+	plt.grid(True)
+	figname = images_dir + 'figure_1_Эффективность элерона на переднем крыле.png' 
+	plt.savefig(figname)
+
+	plt.figure(1)
+	plt.title('Эффективность руля высоты на переднем крыле')
+	plt.ylabel('eff_oz')
+	plt.xlabel('L2/L1')
+	plt.grid(True)
+	figname = images_dir + 'figure_2_Эффективность руля высоты на переднем крыле.png' 
+	plt.savefig(figname)
+
+	plt.figure(2)
+	plt.title('Эффективность элерона на заднем крыле')
+	plt.ylabel('eff_ox')
+	plt.xlabel('L2/L1')
+	plt.grid(True)
+	figname = images_dir + 'figure_3_Эффективность элерона на заднем крыле.png' 
+	plt.savefig(figname)
+
+	plt.figure(3)
+	plt.title('Эффективность руля высоты на заднем крыле')
+	plt.ylabel('eff_oz')
+	plt.xlabel('L2/L1')
+	plt.grid(True)
+	figname = images_dir + 'figure_4_Эффективность руля высоты на заднем крыле.png' 
+	plt.savefig(figname)
+
+def genMXMZFigs(data):
+
+	images_dir = getProjectDir() + 'data/images/mxmz_delta/' #директория, в которую записать графики
+	mkdir(images_dir)
+
+	for item in data: #data[:int(len(data)/4) * 4 : int(len(data)/4)] #делаем 8 графиков для разных случаев
+		plt.figure()
+		plt.suptitle('Значения к-ов для:положение руля=' + str(item['rudpos']) + 
+			'\nL2/L1=' + str(item['backwingspan']/1.25) +
+			'\nОтносительный размах руля=' + str(item['ruderratio']) + str('%'))
+		plt.xlabel('Delta')
+		plt.ylabel('Mx, Mz')
+		plt.grid(True)		
+		#Далее создаем массивы, содержащие значения коэф-ов при линейной аппроксимации
+
+		mx = [np.polyfit(item['aprx_d'], item['aprx_mx'], 1)[0] * x + np.polyfit(item['aprx_d'], item['aprx_mx'], 1)[1] for x in item['aprx_d']]
+		mz = [np.polyfit(item['aprx_d'], item['aprx_mz'], 1)[0] * x + np.polyfit(item['aprx_d'], item['aprx_mz'], 1)[1] for x in item['aprx_d']]
+		plt.plot(item['d'], item['mx'], 'rx')
+		plt.plot(item['aprx_d'], mx, 'r-', label='Mx')		
+		plt.plot(item['d'], item['mz'], 'bx')
+		plt.plot(item['aprx_d'], mz, 'b-', label='Mz')
+		plt.legend()
+
+		plt.savefig(images_dir + 'rudpos='+ str(item['rudpos']) + 
+			' spanratio=' + str(item['backwingspan']/1.25) +
+			' ruderratio=' + str(item['ruderratio']) + '.png')
 
 class Figure:
 	'''
@@ -174,11 +276,12 @@ class Figure:
 			plt.show()
 
 
+
 if __name__ == '__main__':
 
 	oupath = getProjectDir() + 'data\\ou_files\\' #папка с выходными файлами
 	filelist = [f for f in listdir(oupath) if isfile(join(oupath, f))] #получаем список файлов
-	filelist.sort(key=lambda x: os.path.getctime(oupath + x)) #Сортируем по дате создания
+	#filelist.sort(key=lambda x: os.path.getctime(oupath + x)) #Сортируем по дате создания
 	
 	data = [] #словарь с результатом парсинга выходных файлов
 
@@ -204,11 +307,20 @@ if __name__ == '__main__':
 							'ruderratio' : d['ruderratio'],
 							'delta' : d['delta'],
 							'coefs' : d['coefs']})
-	
+	#Считаем еффективность руля:
 	data = calcEfficiency(data)
 
-	'''
+	#iprint(sortByGroup(data, ['rudpos', 'ruderratio'], split=True), True)
+	data = sortByGroup(data, ['rudpos', 'ruderratio'])
+
+	
+	'''	
 	Далее генерируем графики
+	'''
+	genMainFigs(data)
+
+	genMXMZFigs(data)
+
 	'''
 	figures = []
 
@@ -264,9 +376,9 @@ if __name__ == '__main__':
 		fig.plot(figname)
 
 	#генерируем графики коэф-ов моментов от угла отклонения
+	'''
 
-
-
+	'''
 	for item in data: #data[:int(len(data)/4) * 4 : int(len(data)/4)] #делаем 8 графиков для разных случаев
 		plt.figure()
 		plt.suptitle('Значения к-ов для:положение руля=' + str(item['rudpos']) + 
@@ -288,3 +400,4 @@ if __name__ == '__main__':
 		plt.savefig(data_dict + 'mxmz_delta/' + 'rudpos='+ str(item['rudpos']) + 
 			' spanratio=' + str(item['backwingspan']/1.25) +
 			' ruderratio=' + str(item['ruderratio']) + '.png')
+	'''
